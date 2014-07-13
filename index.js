ddcea71f496159f13e83518633f203d225e425d8
Ivo Wetzel <ivo.wetzel@googlemail.com>
2014-07-13 03:19:26 +0200

    First successful compilation of the core test rom

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var opts = troll.options(function(troll) {
     troll.opt('outfile', 'The name of the output rom file', { short: 'o', default: 'game.gb' });
     troll.opt('listfile', 'Name of the assembly listing file to be generated', { short: 'l' });
     troll.opt('mapfile', 'Name of the ROM mapping file to be generated', { short: 'm' });
-    troll.opt('symfile', 'Name of the symbol map file to be generated', { short: 's' });
+    troll.opt('symfile', 'Name of the symbol map file to be generated', { short: 's', default: '' });
     troll.opt('stdout', 'Write any output to standard out instead of a file', { default: false });
     troll.opt('silent', 'Do not produce any logging output', { short: 'S', default: false });
     troll.opt('warnings', 'Enable compiler warnings', { short: 'w', default: false });
@@ -30,9 +30,25 @@ var c = new Compiler(),
 // Mode -----------------------------------------------------------------------
 if (argv.length) {
 
+    console.log(opts);
+
     c.compile(argv);
     c.link();
-    fs.writeFileSync(opts.outfile, c.generate());
+
+    var a = c.generate();
+    //c.optimize();
+
+    var b = c.generate();
+    //console.log(c.symbols());
+    console.log(a.slice(0x200));
+    console.log(b.slice(0x200));
+    fs.writeFileSync(opts.outfile, a);
+
+    if (opts.symfile) {
+        var symbols = c.symbols();
+        //console.log(symbols);
+        fs.writeFileSync(opts.symfile, symbols);
+    }
 
     // TODO generate list map and symfiles
     // TODO respect silent flag
