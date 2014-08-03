997c1a538a0f81170a523833482428920a813083
Ivo Wetzel <ivo.wetzel@googlemail.com>
2014-08-03 18:22:45 +0200

    Remove trollopt requirement

--- index.js
+++ index.js
@@ -1,78 +1,163 @@
 // Dependencies ---------------------------------------------------------------
 // ----------------------------------------------------------------------------
-var troll = require('troll-opt'),
-    fs = require('fs'),
+var fs = require('fs'),
     Compiler = require('./lib/Compiler');
 
 
 // Command Line Interface------------------------------------------------------
 // ----------------------------------------------------------------------------
-var cmd = new troll.Troll();
-var opts = cmd.options(function(troll) {
+var args = process.argv.slice(2),
+    options = {
+        outfile: 'game.gb',
+        optimize: false,
+        mapfile: null,
+        symfile: null,
+        silent: false,
+        version: false,
+        verbose: false,
+        help: false,
+        files: []
+    };
 
-    troll.banner('A JavaScript Gameboy Assembler');
-    troll.opt('outfile', 'The name of the output rom file', { short: 'o', default: 'game.gb' });
-    troll.opt('mapfile', 'Name of the ROM mapping file to be generated', { short: 'm', default: '' });
-    troll.opt('symfile', 'Name of the symbol map file to be generated', { short: 's', default: '' });
-    troll.opt('silent', 'Do not produce any logging output', { short: 'S', default: false });
-    troll.opt('optimize', 'Set optimization level', { short: 'O', default: 0 });
-    troll.opt('warnings', 'Enable compiler warnings', { short: 'w', default: false });
-    troll.opt('verbose', 'Turn on verbose logging', { short: 'v', default: false });
-    troll.opt('version', 'Version information', { short: 'V', default: false });
+for(var i = 0, l = args.length; i < l; i++) {
+    var arg = args[i];
+    switch(arg) {
+        case '-o':
+        case '--outfile':
+            options.outfile = getString(arg, args, ++i);
+            break;
 
-});
+        case '-O':
+        case '--optimize':
+            options.optimize = true;
+            break;
 
+        case '-m':
+        case '--mapfile':
+            options.mapfile = getString(arg, args, ++i);
+            break;
 
-var argv = cmd.argv();
-if (argv.length) {
+        case '-s':
+        case '--symfile':
+            options.symfile = getString(arg, args, ++i);
+            break;
+
+        case '-S':
+        case '--silent':
+            options.silent = true;
+            break;
+
+        case '--version':
+            options.version = true;
+            break;
+
+        case '-v':
+        case '--verbose':
+            options.verbose = true;
+            break;
+
+        case '--help':
+            options.help = true;
+            break;
+
+        default:
+            if (arg.substring(0, 1) === '-') {
+                error('Unknown option: ' + arg);
+
+            } else {
+                options.files.push(arg);
+            }
+            break;
+
+    }
+}
+
+// Version Information
+if (options.version) {
+    var json = JSON.parse(fs.readFileSync('./package.json').toString());
+    process.stdout.write('v' + json.version + '\n');
+
+} else if (options.help) {
+    usage();
+
+// Compile Files
+} else if (options.files.length) {
 
     // Compile
-    var c = new Compiler(opts.silent);
-    c.compile(argv);
+    var c = new Compiler(options.silent);
+    c.compile(options.files);
 
     // Optimize
-    if (opts.optimize > 0) {
-        c.optimize(opts.optimize);
+    if (options.optimize) {
+        c.optimize();
     }
 
     // Generate ROM image
     var rom = c.generate();
-    if (opts.outfile === 'stdout') {
+    if (options.outfile === 'stdout') {
         process.stdout.write(rom);
 
     } else {
-        fs.writeFileSync(opts.outfile, rom);
+        fs.writeFileSync(options.outfile, rom);
     }
 
     // Generate symbol file
-    if (opts.symfile) {
-        if (opts.symfile === 'stdout') {
+    if (options.symfile) {
+        if (options.symfile === 'stdout') {
             process.stdout.write(c.symbols(true));
 
         } else {
-            fs.writeFileSync(opts.symfile, c.symbols(false));
+            fs.writeFileSync(options.symfile, c.symbols(false));
         }
     }
 
     // Generate
-    if (opts.mapfile) {
-        if (opts.mapfile === 'stdout') {
+    if (options.mapfile) {
+        if (options.mapfile === 'stdout') {
             process.stdout.write(c.mapping(true));
 
         } else {
-            fs.writeFileSync(opts.mapfile, c.mapping(false));
+            fs.writeFileSync(options.mapfile, c.mapping(false));
         }
     }
 
 
-} else if (opts.version) {
-    // Version Information
-    var json = JSON.parse(fs.readFileSync('./package.json').toString());
-    process.stdout.write(json.name + ' v' + json.version + '\n');
-
-
+// Usage
 } else {
-    // Usage
-    cmd.usage();
+    usage();
+}
+
+
+// Helpers --------------------------------------------------------------------
+function getString(name, args, index) {
+
+    var s = args[index];
+    if (s === undefined || s.substring(0, 1) === '-') {
+        error('Expected string argument for ' + name);
+
+    } else {
+        return s;
+    }
+
+}
+
+function usage() {
+    process.stdout.write([
+        'Usage: gbasm [options] [sources]',
+        '',
+        '   --outfile, -o <s>: The name of the output rom file (default: game.gb)',
+        '      --optimize, -O: Enable instruction optimizations',
+        '   --mapfile, -m <s>: Name of the ROM mapping file to be generated',
+        '   --symfile, -s <s>: Name of the symbol map file to be generated ',
+        '        --silent, -S: Do not produce any logging output ',
+        '       --verbose, -v: Turn on verbose logging ',
+        '       --version, -V: Display version information ',
+        '              --help: Display this help text'
+    ].join('\n') + '\n');
+}
+
+function error(message) {
+    process.stdout.write('Error: ' + message + '\n');
+    process.exit(1);
 }
 
